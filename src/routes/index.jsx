6903dbe4c64b505796b5c302e748896cbf16f650
charmeee <mandugohigh@gmail.com>
2025-05-29 18:02:33 +0900

    feat: react boilerplate it contains.... - vite - react-router-dom - msw - mui

---
+++ src/routes/index.jsx
@@ -0,0 +1,31 @@
+import {useSelector} from 'react-redux'
+import {Navigate, Outlet, Route, Routes} from 'react-router-dom'
+import BookDetail from '../pages/BookDetail'
+import BookForm from '../pages/BookForm'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+
+const ProtectedRoute = () => {
+  const {isAuthenticated} = useSelector((state) => state.auth)
+  return isAuthenticated ? <Outlet/> : <Navigate to="/login" replace/>
+}
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route element={<ProtectedRoute/>}>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/books">
+          <Route path=":id" element={<BookDetail/>}/>
+          <Route path="write" element={<BookForm/>}/>
+          <Route path=":id/edit" element={<BookForm/>}/>
+        </Route>
+      </Route>
+    </Routes>
+  )
+}
+
+export default AppRoutes 
