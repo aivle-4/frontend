1c429346a59a0774cfe4a02a0eb33acb182f32e7
charmeee <mandugohigh@gmail.com>
2025-05-29 18:38:44 +0900

    feat: 태스트용 간단한 로그인 기능 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
-function Home() {
+import {useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {logout} from '../store/authSlice'
 
+function Home() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   return (
     <div>
       <h1>Home</h1>
+      {/*logout*/}
+      <button onClick={() => {
+
+        dispatch(logout())
+        navigate('/login')
+
+      }}>로그아웃
+      </button>
     </div>
   )
 }
