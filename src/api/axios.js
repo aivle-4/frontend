ed3716cb8e32f84e8ae01253d4162440c99d7b2b
charmeee <mandugohigh@gmail.com>
2025-06-02 12:02:09 +0900

    fix: 401에러시 로그아웃 처리 - 이전에 요청에 넣어놧엇음. 응답에 넣어놔야함

--- src/api/axios.js
+++ src/api/axios.js
@@ -15,13 +15,22 @@ api.interceptors.request.use((config) => {
     config.headers['Authorization'] = `Bearer ${token}`
   }
   return config
-}, (error) => {
-  // 에러 일괄 처리가능
-  // unauthorized 에러 발생 시 token 삭제
-  if (error.response && error.response.status === 401) {
-    store.dispatch(logout())
-  }
-  return Promise.reject(error)
 })
+
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    // 에러 일괄 처리가능
+    // unauthorized 에러 발생 시 token 삭제
+    if (error.response && error.response.status === 401) {
+      console.error('Unauthorized request 로그아웃')
+      store.dispatch(logout())
+    }
+    return Promise.reject(error)
+
+  }
+)
 
 export default api 
